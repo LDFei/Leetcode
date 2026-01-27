c96a48a4fd7156110469e4c08f9eb4e1f335c860
Buckwheat <2319887702@qq.com>
2026-01-27 21:23:27 +0800

    2026-1-27每日刷题

--- 2026-1-27.java
+++ 2026-1-27.java
@@ -27,3 +27,21 @@ class Solution {
         return;
     }
 }
+
+
+//更简洁的代码：
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        for(int i = 0;i < nums.length;i ++)
+        {
+            if(nums[i] != 0)
+            {
+                int a = nums[i];
+                nums[i] = nums[l];
+                nums[l] = a;
+                l ++;
+            }
+        }
+    }
+}
