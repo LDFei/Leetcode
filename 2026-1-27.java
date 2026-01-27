9dd47275fb6d9f7963c5673bdfe3e1adeb067ca2
Buckwheat <2319887702@qq.com>
2026-01-27 21:19:27 +0800

    2026-1-27每日刷题

---
+++ 2026-1-27.java
@@ -0,0 +1,29 @@
+//https://leetcode.cn/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0,r = 0;
+
+        while(l < nums.length && r < nums.length)
+        {
+            if(nums[l] != 0)
+            {
+                l ++;
+                r ++;
+            }
+            else if(nums[r] == 0)
+            {
+                r ++;
+            }
+            else if(nums[l] == 0 && nums[r] != 0)
+            {
+                int a = nums[l];
+                nums[l] = nums[r];
+                nums[r] = a;
+                l ++;
+                r ++;
+            }
+        }
+        return;
+    }
+}
