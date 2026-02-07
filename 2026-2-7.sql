9dca8b94c1411ae876d1a191e8b55159a2e582c8
Buckwheat <2319887702@qq.com>
2026-02-07 23:21:14 +0800

    2026-2-7 SQL练习

---
+++ 2026-2-7.sql
@@ -0,0 +1,22 @@
+--https://leetcode.cn/problems/students-and-examinations/?envType=study-plan-v2&envId=sql-free-50
+SELECT
+    s.student_id,
+    s.student_name,
+    su.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM
+    Students AS s
+        JOIN
+    Subjects AS su
+        LEFT JOIN
+    Examinations AS e
+    ON
+        e.student_id = s.student_id
+            AND
+        e.subject_name = su.subject_name
+GROUP BY
+    s.student_id,
+    su.subject_name
+ORDER BY
+    s.student_id,
+    su.subject_name
