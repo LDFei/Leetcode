74a239e89a89cace7afdea8913e7491d0618c8d7
Buckwheat <2319887702@qq.com>
2026-02-27 11:22:40 +0800

    2026-2-27 JAVA每日练习

--- 2026-2-27.java
+++ 2026-2-27.java
@@ -1,14 +1,5 @@
 //https://leetcode.cn/problems/reverse-linked-list/?envType=study-plan-v2&envId=leetcode-75
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//反转链表，递归写法
 class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null)
@@ -19,5 +10,21 @@ class Solution {
         head.next.next = head;
         head.next = null;
         return cur;
+    }
+}
+
+//反转链表，迭代写法
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null)
+        {
+            ListNode m = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = m;
+        }
+        return pre;
     }
 }
