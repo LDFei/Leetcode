5de0c6761652e1d64bbfb802c2ed75f1eb11c22d
Buckwheat <2319887702@qq.com>
2026-03-20 17:54:54 +0800

    2026-4-1 JAVA每日一题

--- 2026_3_19.java
+++ 2026_3_19.java
@@ -2,27 +2,30 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         Arrays.sort(potions);
-        for (int i = 0; i < spells.length; i++) {
-            spells[i] = potions.length - lowerBound(potions, (double) success / spells[i]);
+        int [] ans = new int [spells.length];
+        for(int i = 0;i < spells.length;i ++)
+        {
+            ans[i] = tool(spells[i],potions,success) - 1;
         }
-        return spells;
+        return ans;
     }
 
-    // 返回 nums 中的第一个大于等于 target 的元素下标
-    private int lowerBound(int[] nums, double target) {
-        int left = -1, right = nums.length; // 开区间 (left, right)
-        while (left + 1 < right) { // 区间不为空
-            // 循环不变量：
-            // nums[left] <= target
-            // nums[right] > target
-            int mid = (left + right) >>> 1; // 比 left+(right-left)/2 更快的写法
-            if (nums[mid] >= target) {
-                right = mid; // 二分范围缩小到 (left, mid)
-            } else {
-                left = mid; // 二分范围缩小到 (mid, right)
+    public int tool(int val,int [] potions,long success)
+    {
+        int l = 0,r = potions.length - 1;
+        int ans = 0;
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if((long)val * potions[mid] < success)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
             }
         }
-        return right;
+        return potions.length - r;
     }
-
 }
